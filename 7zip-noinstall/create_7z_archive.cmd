71b16561c210fbc0d331e3657dfb8bff05e35cb5
Peter Beck <peter.beck@foo.li>
2013-09-26 18:22:32 +0300

    batch file to create 7zip sfx version

---
+++ 7zip-noinstall/create_7z_archive.cmd
@@ -0,0 +1,12 @@
+@echo off
+echo creating 7Zip archive of gitso folder
+echo.
+"%PROGRAMFILES%\7-Zip\7z.exe" a C:\temp\foo-support.7z "%PROGRAMFILES%\Gitso\*.*"
+echo starting 7Zip SFX Maker
+echo.
+::echo don't forget to add your icon to \resources
+echo don't forget to load the correct settings.xml
+copy /y ..\foocube.ico "%PROGRAMFILES%\7-Zip SFX Maker\resources"
+copy /y foo-support_sfxmaler.xml "%PROGRAMFILES%\7-Zip SFX Maker\Config"
+echo.
+"%PROGRAMFILES%\7-Zip SFX Maker\7-ZIP SFX Maker.exe" C:\temp\foo-support.7z 
