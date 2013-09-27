71fa9ed930b06d603c5a8587d6faa0c8d1e17e09
Peter Beck <peter.beck@foo.li>
2013-09-28 02:12:07 +0300

    moved to scripts, added applescript folder

--- foo-scripts/7zip-noinstall/create_7z_archive.cmd
+++ foo-scripts/7zip-noinstall/create_7z_archive.cmd
@@ -6,7 +6,7 @@ echo starting 7Zip SFX Maker
 echo.
 ::echo don't forget to add your icon to \resources
 echo don't forget to load the correct settings.xml
-copy /y ..\foocube.ico "%PROGRAMFILES%\7-Zip SFX Maker\resources"
+copy /y ..\..\foocube.ico "%PROGRAMFILES%\7-Zip SFX Maker\resources"
 copy /y foo-support_sfxmaler.xml "%PROGRAMFILES%\7-Zip SFX Maker\Config"
 echo.
 "%PROGRAMFILES%\7-Zip SFX Maker\7-ZIP SFX Maker.exe" C:\temp\foo-support.7z 
